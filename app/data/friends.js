ba6e341c981f120b83e3cf48d2672f61c00cfb8b
Forrest Eastland <feastland4@gmail.com>
2017-11-02 17:49:39 -0500

    fixed small bug

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArray = [
 	{
 	  name:"Bruce",
-	  photo:"https://i.pinimg.com/736x/e3/9d/27/e39d27bb10d396f4e654c69913b8f4cf--batman-christian-bale-christian-grey.jpg"
+	  photo:"https://i.pinimg.com/736x/e3/9d/27/e39d27bb10d396f4e654c69913b8f4cf--batman-christian-bale-christian-grey.jpg",
 	  scores:[
 	      5,
 	      1,
