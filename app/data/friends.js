70d7914729453a5d4886b75f3bbc94d6e2b9049a
Forrest Eastland <feastland4@gmail.com>
2017-11-02 15:40:35 -0500

    need to fix questions, add sorting logic, and host on heroku

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+	{
+	  name:"Bruce",
+	  photo:"https://i.pinimg.com/736x/e3/9d/27/e39d27bb10d396f4e654c69913b8f4cf--batman-christian-bale-christian-grey.jpg",
+	  scores:[
+	      5,
+	      1,
+	      4,
+	      4,
+	      5,
+	      1,
+	      2,
+	      5,
+	      4,
+	      1
+	    ]
+	}
+];
+
+module.exports = friendsArray;
